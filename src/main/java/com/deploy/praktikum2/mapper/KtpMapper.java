b3a72fc4118c81e7b21c60318dc3ecadf5be9bc1
ZakiAndika9 <m.zaki.andika9@gmail.com>
2026-03-14 13:55:06 +0800

    menambahkan interface KtpMapper

---
+++ src/main/java/com/deploy/praktikum2/mapper/KtpMapper.java
@@ -0,0 +1,13 @@
+package com.deploy.praktikum2.mapper;
+
+import com.deploy.praktikum2.model.dto.KtpDto;
+import com.deploy.praktikum2.model.entity.Ktp;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+
+public interface KtpMapper {
+    KtpMapper MAPPER = Mappers.getMapper(KtpMapper.class);
+    KtpDto toKtpDtoData(Ktp ktp);
+}
